367d9ff049e13b2a920524681f71f977bf773217
Danil <47692381+SelDanilEv@users.noreply.github.com>
2024-06-13 20:23:06 +0400

    commit changes

--- src/dockerDeploymentDev.bat
+++
@@ -1,9 +0,0 @@
-docker rm -f DevIdentityService
-docker build . -t dev-identity-service && ^
-docker run -d --name DevIdentityService -p 49050:80 ^
---env-file ./../../secrets/secrets.dev.list ^
--e ASPNETCORE_ENVIRONMENT=DockerDev ^
--it dev-identity-service
-echo finish dev-identity-service
-docker image prune -f
-pause
